88082bd4d1d357ed98e3ad4fe38d471079d99678
Mag1sterY0da <sashabro2002@gmail.com>
2022-12-09 05:32:53 +0200

    Possible fix

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-export const URL_GEOCODE = 'http://api.positionstack.com/v1/';
+export const URL_GEOCODE = 'https://cors-anywhere.herokuapp.com/http://api.positionstack.com/v1/';
 export const KEY_GEOCODE = 'a86ceed9b6ab3b4e4e0ba309ba3c2ac7';
 export const URL_WEATHER = 'https://api.openweathermap.org/data/2.5/';
 export const KEY_WEATHER = '8cd7d5582aa9b3ff1f5395214f2adb2a';
